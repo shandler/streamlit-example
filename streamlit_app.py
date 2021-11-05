af60519937eec7e42e0c4de890391c360d74a444
shandler <cshandler@gmail.com>
2021-11-05 12:06:51 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,36 +3,25 @@ import altair as alt
 import math
 import pandas as pd
 import streamlit as st
+progress_bar = st.progress(0)
+status_text = st.empty()
+chart = st.line_chart(np.random.randn(10, 2))
 
-"""
-# Welcome to Streamlit!
+for i in range(100):
+    # Update progress bar.
+    progress_bar.progress(i + 1)
 
-Edit `/streamlit_app.py` to customize this app to your heart's desire :heart:
+    new_rows = np.random.randn(10, 2)
 
-If you have any questions, checkout our [documentation](https://docs.streamlit.io) and [community
-forums](https://discuss.streamlit.io).
+    # Update status text.
+    status_text.text(
+        'The latest random number is: %s' % new_rows[-1, 1])
 
-In the meantime, below is an example of what you can do with just a few lines of code:
-"""
+    # Append data to the chart.
+    chart.add_rows(new_rows)
 
+    # Pretend we're doing some computation that takes time.
+    time.sleep(0.1)
 
-with st.echo(code_location='below'):
-    total_points = st.slider("Number of points in spiral", 1, 5000, 2000)
-    num_turns = st.slider("Number of turns in spiral", 1, 100, 9)
-
-    Point = namedtuple('Point', 'x y')
-    data = []
-
-    points_per_turn = total_points / num_turns
-
-    for curr_point_num in range(total_points):
-        curr_turn, i = divmod(curr_point_num, points_per_turn)
-        angle = (curr_turn + 1) * 2 * math.pi * i / points_per_turn
-        radius = curr_point_num / total_points
-        x = radius * math.cos(angle)
-        y = radius * math.sin(angle)
-        data.append(Point(x, y))
-
-    st.altair_chart(alt.Chart(pd.DataFrame(data), height=500, width=500)
-        .mark_circle(color='#0068c9', opacity=0.5)
-        .encode(x='x:Q', y='y:Q'))
+status_text.text('Done!')
+st.balloons()
